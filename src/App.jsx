36f32cdbc4f9268f92c27be9f86f8ca0fc5fdf06
vyom Priyesh <vyompriyesh@gmail.com>
2025-07-03 10:17:46 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from "./Component/Header"
 import Footer from "./Component/Footer"
 import BlogDetails from "./Pages/BlogDetails"
-import Home from "./Pages/home"
+import LandingPage from "./Pages/LandingPage"
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <div className="container mx-auto px-5 mb-5">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<LandingPage />} />
             <Route path="/blog-details" element={<BlogDetails />} />
           </Routes>
         </div>
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroCard from '../Component/HeroCard'
+import Blog from '../Component/Blog'
+
+const LandingPage = () => {
+  return (
+   <>
+        <HeroCard />
+        <Blog />  
+      </>
+  )
+}
+
+export default LandingPage
